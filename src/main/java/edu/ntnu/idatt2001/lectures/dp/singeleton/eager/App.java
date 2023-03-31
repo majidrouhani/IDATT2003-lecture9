27f39faf631ac3c2beb6c4946f5efacab7098fbb
majidrouhanintnu <42731301+majidrouhanintnu@users.noreply.github.com>
2023-03-31 10:54:30 +0200

    bugix: renamed getA to getInstance

--- src/main/java/edu/ntnu/idatt2001/lectures/dp/singeleton/eager/App.java
+++ src/main/java/edu/ntnu/idatt2001/lectures/dp/singeleton/eager/App.java
@@ -3,10 +3,10 @@ package edu.ntnu.idatt2001.lectures.dp.singeleton.eager;
 public class App {
 
   public static void main(String... args) {
-    EagerlyInitializedSingleton a = EagerlyInitializedSingleton.getA();
+    EagerlyInitializedSingleton a = EagerlyInitializedSingleton.getInstance();
     System.out.println(a.doSomething());
 
-    EagerlyInitializedSingleton b = EagerlyInitializedSingleton.getA();
+    EagerlyInitializedSingleton b = EagerlyInitializedSingleton.getInstance();
     System.out.println(b.doSomething());
 
     if (a == b) {
